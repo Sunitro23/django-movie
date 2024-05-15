47b83793773e6b3cbd17a4e3f0ed3e1cca258073
Armand <98162703+Sunitro23@users.noreply.github.com>
2024-05-15 10:36:08 +0200

    Model Changes + API Service
    Correction of the ArrayFields in Movie model, now we use Genre.
    Creation of the service API, that makes the calls to the API.

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.template import loader
+from asyncio import Future
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Movie
+from movies.service import get_movie_by_id_db, get_movies_by_title_db
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+def movie_page(request, movie_id):
+    try:
+        get_movies_by_title_db("Star Wars")
+        movie = get_movie_by_id_db(movie_id)
+    except Movie.DoesNotExist:
+        movie = None
+    if movie:
+        return render(request, "movie_page.html", {"movie": movie})
+    else:
+        return HttpResponse("No movie found.")
