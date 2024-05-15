7b0c01a4ccba41b876dc67856320bdbc88dd04cf
Basile Mercado <mercbazz@gmail.com>
2024-05-15 09:36:07 +0200

    add movies model + form page

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Movies(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    year = models.DateField(help_text='Release year')
+    genre = ArrayField(models.CharField(max_length=100))
+    poster = models.CharField()
